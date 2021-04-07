7a059b8f6d9e8f7d9cbd87eab32af4fea2873fe4
0918nobita <nobita.0918@gmail.com>
2021-04-07 12:16:03 +0900

    :construction: C++ に移植してみた
    でも実機で起動すると何も表示されない

--- main.cc
+++ main.cc
@@ -1,7 +1,7 @@
+using Char16 = unsigned short;
 using EfiStatus = unsigned long long;
 using EfiHandle = void*;
-
-using EfiTextString = EfiStatus (*) (struct EfiSimpleTextOutputProtocol *This, const char *string);
+using EfiTextString = EfiStatus (*) (struct EfiSimpleTextOutputProtocol *This, Char16 *string);
 
 struct EfiSimpleTextOutputProtocol {
     void *dummy;
@@ -16,7 +16,7 @@ struct EfiSystemTable {
 
 EfiStatus EfiMain(EfiHandle imageHandle, EfiSystemTable *systemTable) {
     (void) imageHandle;
-    systemTable->conOut->outputString(systemTable->conOut, "Hello, world!\n");
+    systemTable->conOut->outputString(systemTable->conOut, (unsigned short *) L"Hello, world!\n");
     while (1);
     return 0;
 }
